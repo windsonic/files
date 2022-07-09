a946b6cfc34f4c36dc522489b3989480c374df68
windsonic <jarod.zhang@live.cn>
2022-07-09 20:13:38 +0800

    Update tooltip-link.js

--- tooltip-link.js
+++ tooltip-link.js
@@ -5,10 +5,18 @@ if (sap && sap.importAddOnExtensions) {
             description: 'Add links on chart tooltip',
             factory: function () {
                 return {
-                    getTooltipLink: function (title, value) {
+                    getTooltipLink: function (header, details) {
+                        var headerEntities = header.headerEntities || [];
+                        var detailsEntities = header.detailsEntities || [];
+                        var detailsPath = detailsEntities
+                            .map(function (entry) { return entry.value; })
+                            .reduce(function (result, curItem) {
+                            return "".concat(result, "/").concat(curItem);
+                        }, '');
+                        var label = headerEntities[0] ? headerEntities[0].title : 'Details Link';
                         return {
-                            url: "http://www.sap.com/".concat(value ? value : ''),
-                            label: "".concat(title ? title : 'Link')
+                            url: "http://www.sap.com/".concat(detailsPath),
+                            label: label
                         };
                     }
                 };
